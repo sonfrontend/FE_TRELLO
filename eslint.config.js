26b15b8d8d26c8989ea145a694fc1fed34ac449a
sonfrontend <sonfrontend@gmail.com>
2026-03-06 22:34:29 +0700

    Test:lint-staged và husky

--- eslint.config.js
+++ eslint.config.js
@@ -1,47 +1,51 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-import eslintConfigPrettier from 'eslint-config-prettier/flat';
-import prettier from 'eslint-plugin-prettier';
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import prettier from "eslint-plugin-prettier";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended,
       eslintConfigPrettier,
     ],
-    files: ['**/*.{ts,tsx,jsx}'],
+    files: ["**/*.{ts,tsx,jsx}"],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser
+      globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      prettier: prettier
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      prettier: prettier,
     },
     rules: {
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      '@typescript-eslint/no-unused-vars': 'error',
       ...reactHooks.configs.recommended.rules,
-      'prettier/prettier': [
-        'error',
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "@typescript-eslint/no-unused-vars": "error",
+      quotes: ["error", "single"],
+      "prettier/prettier": [
+        "error",
         {
-          arrowParens: 'always',
+          arrowParens: "always",
           semi: true,
-          trailingComma: 'none',
+          trailingComma: "none",
           tabWidth: 2,
-          endOfLine: 'auto',
+          endOfLine: "auto",
           useTabs: false,
           singleQuote: true,
           printWidth: 120,
-          jsxSingleQuote: true
-        }
-      ]
-    }
-  }
+          jsxSingleQuote: true,
+        },
+      ],
+    },
+  },
 );
